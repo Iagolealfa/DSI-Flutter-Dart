346cc2d4788e83049301eb91c4dc43a533296d20
Iago Leal <70781193+Iagolealfa@users.noreply.github.com>
2022-03-06 15:10:06 -0300

    Update main.dart

--- topico1/lib/main.dart
+++ topico1/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Startup Name Generato',
       home: RandomWords(),
     );
   }
